962d91184265b02c264e6d3115c7c12460db21cc
artnovak <artnovak1993@gmail.com>
2022-01-11 11:37:12 +1000

    cgi version

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import datetime
 
 def do_magic():
     now = datetime.datetime.now()
-    return "Hello! {0}" .format(now)
+    return "Hello! {0}".format(now)
 
 if __name__ == "__main__":
+    print("Content-type: text/html\n\n") 
     print(do_magic()) 
